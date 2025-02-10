7798d34f437240168a15a005408ddd73df7eb4cd
dlcastra <1dlcastra@gmail.com>
2025-02-10 16:57:52 +0100

    File scraping

--- app/routers/converters.py
+++ app/routers/converters.py
@@ -1,20 +1,32 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from app.handlers import convert_file
+from app.handlers import convert_file, file_scraper
 from app.utils import callback
 
 router = APIRouter()
 
 
-class RequestS3Key(BaseModel):
+class ConvertFileRequest(BaseModel):
     s3_key: str
     format_from: str
     format_to: str
     callback_url: str
 
 
+class FileParsingRequest(BaseModel):
+    s3_key: str
+    keywords: list[str]
+    callback_url: str
+
+
 @router.post("/convert-file")
-async def convert_from_docx_to_pdf(request: RequestS3Key):
+async def convert_from_docx_to_pdf(request: ConvertFileRequest):
     status, result = await convert_file(request.s3_key, request.format_from, request.format_to)
     return await callback(request.callback_url, status=status, data=result)
+
+
+@router.post("/parse-file")
+async def parse_file(request: FileParsingRequest):
+    status, result = await file_scraper(request.s3_key, request.keywords)
+    return await callback(request.callback_url, status=status, data={"result": result})
---
+++ app/services/scraper.py
@@ -0,0 +1,79 @@
+import asyncio
+import re
+import tempfile
+
+import aiofiles
+import fitz
+from docx import Document
+from rapidfuzz import fuzz
+
+from app.utils import download_file
+
+
+class FileScraperService:
+    def __init__(self):
+        self.keywords = []
+
+    async def file_processing(self, s3_key, download_url, keywords):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            file_path = f"{tmpdir}/{s3_key}"
+            self.keywords = keywords
+
+            message, is_downloaded = download_file(file_path, download_url)
+            if not is_downloaded:
+                return message, None
+            if file_path.endswith(".txt"):
+                return await self.extract_text_from_txt(file_path), True
+            elif file_path.endswith(".docx"):
+                return await self.extract_text_from_docx(file_path), True
+            elif file_path.endswith(".pdf"):
+                return await self.extract_text_from_pdf(file_path), True
+
+            return "Unsupported file type", None
+
+    async def extract_text_from_txt(self, file_path):
+        async with aiofiles.open(file=file_path, mode="r", encoding="utf-8") as file:
+            result = await file.read()
+            return self.find_sentences_with_fuzzy_keywords(result)
+
+    async def extract_text_from_docx(self, file_path):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(None, self._extract_docx, file_path)
+
+    def _extract_docx(self, file_path):
+        doc = Document(file_path)
+        paragraphs = [para.text for para in doc.paragraphs if para.text.strip()]
+
+        found_sentences = []
+        for paragraph in paragraphs:
+            found_sentences.extend(self.find_sentences_with_fuzzy_keywords(paragraph))
+
+        return found_sentences
+
+    async def extract_text_from_pdf(self, file_path):
+        doc = fitz.open(file_path)
+
+        tasks = [asyncio.to_thread(self._sync_extract_page, page) for page in doc]
+        pages_text = await asyncio.gather(*tasks)
+
+        tasks = [asyncio.to_thread(self.find_sentences_with_fuzzy_keywords, page_text) for page_text in pages_text]
+        results = await asyncio.gather(*tasks)
+
+        return [sentence for result in results for sentence in result]
+
+    def _sync_extract_page(self, page):
+        return page.get_text("text")
+
+    def find_sentences_with_fuzzy_keywords(self, text, threshold=80):
+        keywords = [kw.lower() for kw in self.keywords]
+        sentences = re.split(r"(?<=[.!?])\s+", text)
+        found_sentences = []
+
+        for sentence in sentences:
+            words = sentence.lower().split()
+            for keyword in keywords:
+                if any(fuzz.ratio(word, keyword) >= threshold for word in words):
+                    found_sentences.append(sentence)
+                    break
+
+        return found_sentences

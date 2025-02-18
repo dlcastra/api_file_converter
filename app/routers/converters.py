ecf7b8432b2cdabd223ed9076246f1d3c3cdb1bb
dlcastra <1dlcastra@gmail.com>
2025-02-18 14:10:33 +0100

    fix commit

--- app/routers/converters.py
+++ app/routers/converters.py
@@ -22,11 +22,18 @@ class FileParsingRequest(BaseModel):
 
 @router.post("/convert-file")
 async def convert_from_docx_to_pdf(request: ConvertFileRequest):
-    status, result = await convert_file(request.s3_key, request.format_from, request.format_to)
-    return await callback(request.callback_url, status=status, data=result)
+    try:
+        status, result = await convert_file(request.s3_key, request.format_from, request.format_to)
+        return await callback(request.callback_url, status=status, data=result)
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
 
 
 @router.post("/parse-file")
 async def parse_file(request: FileParsingRequest):
-    status, result = await file_scraper(request.s3_key, request.keywords)
-    return await callback(request.callback_url, status=status, data={"result": result})
+    try:
+        status, result = await file_scraper(request.s3_key, request.keywords)
+        result["s3_key"] = request.s3_key
+        return await callback(request.callback_url, status=status, data=result)
+    except Exception as e:
+        return {"status": "error", "message": str(e)}

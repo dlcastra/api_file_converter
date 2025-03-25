b043d3610e1acd23257910050b70ff5fbea1a69e
dlcastra <1dlcastra@gmail.com>
2025-03-25 12:32:03 +0100

    file format constants

---
+++ src/app/constants.py
@@ -0,0 +1,12 @@
+CONTENT_TYPES = {
+    "pdf": "application/pdf",
+    "txt": "text/plain",
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+}
+
+ALLOWED_IMAGES_TYPES = ["png", "jpg", "jpeg"]
+ALLOWED_FILE_FORMATS = ["pdf", "doc", "docx", "txt"]

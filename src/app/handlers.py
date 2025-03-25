227bb78e72b3e82b999fa32f862aaedb6ae1bb49
dlcastra <1dlcastra@gmail.com>
2025-03-25 12:58:25 +0100

    Changes for convert_file for logic with byte code

--- src/app/handlers.py
+++ src/app/handlers.py
@@ -1,13 +1,16 @@
+from typing import Tuple, Dict
+
+from src.app.aws.utils import download_file_as_bytes, upload_bytes_to_s3
 from src.app.models.statuses import Status
 from src.app.services import get_file_scraper_service, get_file_converter_service
-from src.app.typing.converter import ConverterHandler
 from src.app.typing.scraper import ScraperHandler
 from src.settings.config import settings, logger
+from src.app.constants import CONTENT_TYPES
 
 
-async def convert_file(s3_key: str, old_format: str, format_to: str) -> ConverterHandler:
+async def convert_file(s3_key: str, old_format: str, format_to: str) -> Tuple[str, Dict]:
     """
-    Function to convert the file from S3 bucket from one format to another.
+    Function to convert the file as bytes from S3 bucket from one format to another.
     **Returns tuple with the str and the dict if the process was successful,
     otherwise returns tuple with the str and the str.**
 
@@ -24,12 +27,21 @@ async def convert_file(s3_key: str, old_format: str, format_to: str) -> Converte
     try:
         logger.info("File conversion started")
         converted_s3_key = s3_key.replace(f".{old_format}", f".{format_to}")
-        details, is_processed = await converter.file_processing(
-            s3_key=s3_key, converted_key=converted_s3_key, format_to=format_to, bucket=bucket
-        )
+
+        download_result, is_downloaded = await download_file_as_bytes(bucket, s3_key)
+        if not is_downloaded:
+            logger.error(f"File download failed. Details: {download_result}")
+            return Status.ERROR, {"message": download_result}
+
+        conv_result, is_processed = await converter.file_processing(old_format, format_to, download_result)
         if not is_processed:
-            logger.error(f"File conversion failed. Details: {details}")
-            return Status.ERROR, {"message": details}
+            logger.error(f"File conversion failed.")
+            return Status.ERROR, {"message": conv_result}
+
+        message, is_uploaded = await upload_bytes_to_s3(bucket, converted_s3_key, conv_result, CONTENT_TYPES[format_to])
+        if not is_uploaded:
+            logger.error(f"File upload failed. Details: {message}")
+            return Status.ERROR, {"message": message}
 
         file_url = f"https://{bucket}.s3.{region}.amazonaws.com/{converted_s3_key}"
         logger.info("File conversion successful")
